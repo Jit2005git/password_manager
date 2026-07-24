9bd8dc1c85c4b1fa6da044ca650bccb4c6f0df63
Jit2005git <jitghosh304@gmail.com>
2026-07-24 21:33:06 +0530

    passwords3

--- password_manager.py
+++ password_manager.py
@@ -7,3 +7,17 @@ try:
         for line in file:
             website,pwd = line.strip().split(":")
             passwords[website] = pwd
+except :
+    pass
+def generate_password():
+    chars=string.ascii_letters+string.digits+string.punctuation
+    password="".join(random.choice(chars) for i in range(8))
+    return password
+while True:
+    print("\n ------ Password Manager ------")
+    print("1. Generate a new password")
+    print("2. View existing passwords")
+    print("3. Save password")
+    print("4. Exit")
+    choice=input("Enter your choice: ")
+    if choice=="1":
